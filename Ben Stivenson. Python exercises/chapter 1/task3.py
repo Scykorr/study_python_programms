f1e87babccbf808688d25d64c8c474c1121e6399
Anton Fedosov <fedosov-toxa@mail.ru>
2024-01-16 17:07:39 +0300

    task3 done

---
+++ Ben Stivenson. Python exercises/chapter 1/task3.py
@@ -0,0 +1,4 @@
+"""Площадь комнаты"""
+room_lenght = float(input('Введите длину комнаты: '))
+room_width = float(input('Введите ширину комнаты: '))
+print(f'Площадь комнаты: {room_lenght * room_width}')
