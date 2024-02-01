f9a7fcf7e4147a63602c97964ec9bf90332853c5
Anton Fedosov <fedosov-toxa@mail.ru>
2024-02-01 09:29:37 +0300

    task11 done

--- Ben Stivenson. Python exercises/chapter 1/task11.py
+++ Ben Stivenson. Python exercises/chapter 1/task11.py
@@ -1,3 +1,7 @@
 """Упражнение 11. Потребление топлива"""
-miles_per_gallon = float(input('Введите показатель потребляния топлива США: '))
-liters_per_hundred_kilometers = None
+miles_per_gallon = float(
+    input('Введите показатель потребляния топлива США\nМиль на галлон: '))
+liters_per_hundred_kilometers = round(
+    3.78541 * 100 / (miles_per_gallon * 1.60934), 3)
+print(f'Показатель потребления топлива Канады\nЛитров на 100км: {
+      liters_per_hundred_kilometers}')
