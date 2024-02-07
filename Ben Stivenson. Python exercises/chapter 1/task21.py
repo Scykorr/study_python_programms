ffd2f1e8907ed24823399acdb5b33c3cf9672bff
Anton Fedosov <fedosov-toxa@mail.ru>
2024-02-07 14:01:02 +0300

    fix task21

--- Ben Stivenson. Python exercises/chapter 1/task21.py
+++ Ben Stivenson. Python exercises/chapter 1/task21.py
@@ -1,10 +1,11 @@
 """Упражнение 21. Площадь треугольника"""
 
-def calc_sqr_triangle(base: float, height: float) -> None:
-    print(f'Площадь треугольника: {base * height / 2}')
+
+def calc_sqr_triangle(base_triangle: float, height_triangle: float) -> None:
+    print(f'Площадь треугольника: {base_triangle * height_triangle / 2}')
+
 
 if __name__ == '__main__':
     base = float(input('Введите основание треугольника: '))
     height = float(input('Введите высоту треугольника: '))
-    calc_sqr_triangle(base=base, height=height)
-
+    calc_sqr_triangle(base_triangle=base, height_triangle=height)
