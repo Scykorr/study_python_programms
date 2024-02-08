9b5788827887e4ddd4579ea29a03920919c52cfc
Anton Fedosov <fedosov-toxa@mail.ru>
2024-02-08 17:56:26 +0300

    get first line docstring of files in chapter 1

--- support_files/get_docstrings.py
+++ support_files/get_docstrings.py
@@ -1,1 +1,19 @@
 import os
+
+curr_directory = os.getcwd()
+
+print(curr_directory)
+
+
+directory = os.path.join(
+    curr_directory, 'Ben Stivenson. Python exercises\chapter 1')
+
+files = os.listdir(directory)
+
+open(f'{directory}/files_describes.txt', 'w', encoding='utf8').close()
+
+for file in files:
+    if file != 'files_describes.txt':
+        with open(f'{directory}/{file}', 'r', encoding='utf8') as curr_file:
+            with open(f'{directory}/files_describes.txt', 'a', encoding='utf8') as outp_file:
+                outp_file.write(curr_file.readline())
