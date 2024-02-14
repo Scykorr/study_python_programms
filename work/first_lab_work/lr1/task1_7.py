41ed42bb25ee357ce56ea93e3066a5d57d362bd6
Tony <fedosov-toxa@mail.ru>
2024-02-14 20:45:41 +0300

    tasks_part_1 func done

--- work/first_lab_work/lr1/task1_7.py
+++ work/first_lab_work/lr1/task1_7.py
@@ -1,28 +1,37 @@
-# from sympy import Eq, symbols, solve
+from sympy import Eq, symbols, solve
 
-# x, y = symbols("x y")
+x, y = symbols("x y")
 
-# eq1 = Eq(5 * pow(x, 2) + 6 * pow(y, 2), 3)
-# eq2 = Eq(7 * x + 3 * y, 1)
+eq1 = Eq(5 * pow(x, 2) + 6 * pow(y, 2), 3)
+eq2 = Eq(7 * x + 3 * y, 1)
 
-# solution = solve((eq1, eq2), (x, y))
-# print(solution)
+solution = solve((eq1, eq2), (x, y))
+print(solution)
 
-import numpy as np
-import matplotlib.pyplot as plt
-plt.title(r'$Задание 1.7$')
+
+
 # set axis limits of plot (x=0 to 20, y=0 to 20)
-plt.axis([-5, 10, -5, 10])
+import numpy as np
+from matplotlib import pyplot as plt
+from math import pi
 
-# create circle with (x, y) coordinates at (10, 10)
-c = plt.Circle((0, 0))
 
-# add circle to plot (gca means "get current axis")
-plt.gca().add_artist(c)
+u=0     #x-position of the center
+v=0    #y-position of the center
+a=np.sqrt(3/5)     #radius on the x-axis
+b=np.sqrt(3/6)    #radius on the y-axis
+
+t = np.linspace(0, 2*pi, 100)
+x = np.linspace(-1, 1, 200)
+plt.title(r'$Задание 1.7$')
+plt.plot(x, ((1 - 7 * x) / 3))
+plt.plot( u+a*np.cos(t) , v+b*np.sin(t))
 plt.grid(True)
 plt.xlabel(r'$x$')
 plt.ylabel(r'$g$')
 plt.show()
+
+
 
 
 # import numpy as np
