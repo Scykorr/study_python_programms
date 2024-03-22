8d2f98643d4af735a064b4c3fadf45ec56b4c623
Tony <fedosov-toxa@mail.ru>
2024-03-22 18:11:47 +0300

    task33 done

---
+++ Ben Stivenson. Python exercises/chapter 1/task33.py
@@ -0,0 +1,15 @@
+"""Упражнение 33. Сортировка трех чисел"""
+
+
+def get_filter_list(inp_list: list) -> None:
+    max_val = max(inp_list)
+    min_val = min(inp_list)
+    middle_num = sum(inp_list) - max_val - min_val
+    print(f'Отсортированный список: {min_val}, {middle_num}, {max_val}')
+
+
+if __name__ == '__main__':
+    nums_list = []
+    for el in range(1, 4):
+        nums_list.append(int(input(f'Введите число №{el}: ')))
+    get_filter_list(nums_list)
