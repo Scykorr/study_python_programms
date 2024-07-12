5340bb71294c8d0ad674d9ff9c7457117c5061eb
Anton Fedosov <fedosov-toxa@mail.ru>
2024-07-12 15:15:25 +0300

    15_5 in progress

---
+++ stepik/pokolenie_python_begginer/15/15_5/15_5_1.py
@@ -0,0 +1,9 @@
+def cesar_cipher(string, direction, step):
+    pass
+
+
+inp_string = input('Введите текст')
+inp_direction = input(' ')
+inp_step = input(' ')
+input_lang = input(' ')
+cesar_cipher(string=inp_string, direction=inp_direction, step=inp_step)
