c91d8b5cc8360133755284ddbc9c4f9ce545d76d
Tony <fedosov-toxa@mail.ru>
2024-09-13 20:08:14 +0300

    task 36 is done

---
+++ Ben Stivenson. Python exercises/chapter 2/task36.py
@@ -0,0 +1,15 @@
+"Упражнение 36. Собачий возраст"
+
+def count_dog_age(people_age):
+    dog_age = 0
+    if people_age < 0:
+        return "Вы ввели отрицательное число"
+    for age in range(1, people_age + 1):
+        if age <= 2:
+            dog_age += 10.5
+        else:
+            dog_age += 4
+    return dog_age
+
+res_dog_age = count_dog_age(5)
+print('Возраст собаки в расчете на человеческие года:', res_dog_age)
