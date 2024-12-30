4c2a1f24a90ba93a8df5da93c62fc58baba0b5b2
Tony <fedosov-toxa@mail.ru>
2024-12-30 11:24:01 +0300

    task43 is done

---
+++ Ben Stivenson. Python exercises/chapter 2/task43.py
@@ -0,0 +1,29 @@
+""" Упражнение 43. Узнать частоту по ноте"""
+
+
+def main():
+    freq = float(input_freq())
+    if 260.63 <= freq <= 262.63:
+        print('Нота C4')
+    elif 292.66 <= freq <= 294.66:
+        print('Нота D4')
+    elif 328.63 <= freq <= 330.63:
+        print('Нота E4')
+    elif 348.23 <= freq <= 350.23:
+        print('Нота F4')
+    elif 391 <= freq <= 393:
+        print('Нота G4')
+    elif 439 <= freq <= 441:
+        print('Нота A4')
+    elif 492.88 <= freq <= 494.88:
+        print('Нота B4')
+    else:
+        print('Ноты, соответствующей введенной частоте не существует')
+
+
+def input_freq():
+    return input('Введите частоту: ')
+
+
+if __name__ == '__main__':
+    main()
